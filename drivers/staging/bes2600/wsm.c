2851e8d72ae13008f22c820111ea9a8a2f2f997c
Oren Klopfer <oren+12345@taumoda.com>
2023-08-02 23:06:35 +0000

    proper func

--- drivers/staging/bes2600/wsm.c
+++ drivers/staging/bes2600/wsm.c
@@ -134,7 +134,7 @@ static int wsm_generic_confirm(struct bes2600_common *hw_priv,
 				 struct wsm_buf *buf)
 {
 	u32 status = WSM_GET32(buf);
-	if (WARN(status != WSM_STATUS_SUCCESS, "%s ret %u", status))
+	if (WARN(status != WSM_STATUS_SUCCESS, "%s ret %u", __func__, status))
 		return -EINVAL;
 	return 0;
 
@@ -684,7 +684,7 @@ static int wsm_join_confirm(struct bes2600_common *hw_priv,
 	wsm_oper_unlock(hw_priv);
 
 	if (status != WSM_STATUS_SUCCESS) {
-		bes2600_warn(BES2600_DBG_WSM, "wsm_join_confirm ret %u\n", status);
+		bes2600_warn(BES2600_DBG_WSM, "%s ret %u\n", __func__, status);
 		return -EINVAL;
 	}
 
